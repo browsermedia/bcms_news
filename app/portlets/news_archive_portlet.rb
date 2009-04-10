848e07251220c5a7c14c066a659145d0cedf3711
Paul Barry <mail@paulbarry.com>
2009-04-10 13:48:10 -0400

    Added auto-discovery link to news archive portlet

--- app/portlets/news_archive_portlet.rb
+++ app/portlets/news_archive_portlet.rb
@@ -1,11 +1,11 @@
 class NewsArchivePortlet < Portlet
 
   def render
-    if category_id.blank?
-      @articles = NewsArticle.all(:order => "release_date desc", :limit => limit)
+    if self.category_id.blank?
+      @articles = NewsArticle.all(:order => "release_date desc", :limit => self.limit)
     else
-      @category = Category.find(category_id)
-      @articles = NewsArticle.all(:conditions => ["category_id = ?", @category.id], :order => "release_date desc", :limit => limit)
+      @category = Category.find(self.category_id)
+      @articles = NewsArticle.all(:conditions => ["category_id = ?", @category.id], :order => "release_date desc", :limit => self.limit)
     end
   end
 
