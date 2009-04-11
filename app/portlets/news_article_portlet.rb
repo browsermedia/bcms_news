029ccd9dd394e27fd5d94b088c20624aa7808632
Paul Barry <mail@paulbarry.com>
2009-04-11 13:38:45 -0400

    Modified news article portlet to work with page routes

--- app/portlets/news_article_portlet.rb
+++ app/portlets/news_article_portlet.rb
@@ -1,7 +1,10 @@
 class NewsArticlePortlet < Portlet
   
   def render
-    @article = NewsArticle.find(params[:news_article_id]) if params[:news_article_id]
+    # @news_article should already be set by the page route
+    if !@news_article && params[:news_article_id]
+      @news_article = NewsArticle.find(params[:news_article_id])
+    end
   end
     
 end
