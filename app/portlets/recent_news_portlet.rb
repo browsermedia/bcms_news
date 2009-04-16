72f35f950e3759276e2eb0fff186da48bdff060a
ssoper <sean.soper@gmail.com>
2009-04-16 17:06:50 -0400

    adding sort_by and sort_order to news_article portlet

--- app/portlets/recent_news_portlet.rb
+++ app/portlets/recent_news_portlet.rb
@@ -1,11 +1,16 @@
 class RecentNewsPortlet < Portlet
   
   def render
+    order = "release_date DESC"
+    if !@portlet.sort_by.blank? and !@portlet.sort_order.blank?
+      order = "#{@portlet.sort_by} #{@portlet.sort_order}"
+    end
+
     if @portlet.category_id.blank?
-      @articles = NewsArticle.all(:order => "release_date desc", :limit => @portlet.limit)
+      @articles = NewsArticle.all(:order => order, :limit => @portlet.limit)
     else
       @category = Category.find(category_id)
-      @articles = NewsArticle.all(:conditions => ["category_id = ?", @category.id], :order => "release_date desc", :limit => @portlet.limit)
+      @articles = NewsArticle.all(:conditions => ["category_id = ?", @category.id], :order => order, :limit => @portlet.limit)
     end
   end
     
