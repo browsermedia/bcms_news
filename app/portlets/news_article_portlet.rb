bf580a44447d66a3348dc908f069cf27dba1d3ad
crh3675 <crh3675@gmail.com>
2011-05-13 16:24:23 -0400

    Replaced template cms_text_area tags with cms_template_editor tags in Portlet forms. Added method to portlets to enable/disable.  Fixed syntax for auto_discovery tags

--- app/portlets/news_article_portlet.rb
+++ app/portlets/news_article_portlet.rb
@@ -1,4 +1,7 @@
 class NewsArticlePortlet < Portlet
+
+  # Mark this as 'true' to allow the portlet's template to be editable via the CMS admin UI.
+  enable_template_editor false
   
   def render
     # @news_article should already be set by the page route
